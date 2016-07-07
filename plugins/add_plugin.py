cc68bf47b9a84cef4f9a3f1901350e5d08e154c5
Shannon <shannon.babincsak@gmail.com>
2016-07-07 13:09:46 -0500

    AddPlugin now takes in code and prints it

--- plugins/add_plugin.py
+++ plugins/add_plugin.py
@@ -1,9 +1,10 @@
 from will.plugin import WillPlugin
-from will.decorators import respond_to
+from will.decorators import respond_to, hear
 
 class AddPluginPlugin(WillPlugin):
 
-    @respond_to("/bender-add-plugin")
-    def add_plugin(self, message):
+    @hear("^/bender-add-plugin (?P<code>.*)", multiline=True)
+    def add_plugin(self, message, code):
         """add_plugin: Add a plugin to Bender from within HipChat"""
+        print(code)
         self.reply(message, "I'm working on it.")
