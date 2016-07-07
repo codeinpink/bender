edf27c86dcffc9dddb6d8bc96250bc3c3e33b7b1
Shannon <shannon.babincsak@gmail.com>
2016-07-07 13:48:05 -0500

    AddPlugin writes plugin to file

--- plugins/add_plugin.py
+++ plugins/add_plugin.py
@@ -3,11 +3,16 @@ from will.decorators import respond_to, hear
 
 class AddPluginPlugin(WillPlugin):
 
-    @hear("^/bender-add-plugin (?P<code>.*)", multiline=True)
-    def add_plugin(self, message, code):
+    @hear("^/bender-add-plugin (?P<name>.\w+) (?P<code>.*)", multiline=True)
+    def add_plugin(self, message, name, code):
         """add_plugin: Add a plugin to Bender from within HipChat"""
-        print(code)
         self.reply(message, "I'm working on it.")
+
+        with open('plugins/' + name + '.py', 'w+') as f:
+            f.write(code)
+
+        self.reply(message, "Done!")
+
 
     @hear("^/bender-reload$")
     def reload(self, message):
