c2baf4e6c58f26c1b543f36524e23dad843f5c83
Michael Harrison <mgh0010@auburn.edu>
2016-07-07 17:59:09 -0500

    added missing )

--- plugins/quick_add_plugin.py
+++ plugins/quick_add_plugin.py
@@ -25,7 +25,7 @@ class QuickAddPluginPlugin(WillPlugin):
         code = 'from will.plugin import WillPlugin\n' + \
                  'from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings\n\n' + \
                  'class QuickAddPluginPlugin(WillPlugin):\n' + \
-                 '\t@hear("^{} {}$".format(message, name)' 
+                 '\t@hear("^{} {}$".format(message, name))' 
         for line_of_code in code_array:
             code += '\n\t{}'.format(line_of_code)
         return code
